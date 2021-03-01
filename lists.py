dffce9184fef14b58d073b2527ea0f73d766f665
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-03-01 23:04:18 +0100

    basic lists

---
+++ lists.py
@@ -0,0 +1,8 @@
+manga = ["Berserk", "Vagabond", "Beck", "Medaka", "FOTNS", "Kengan"]
+print(manga[2])
+manga[2] = "Bakemonogatari"
+print(manga[0])
+print(manga[-1])  # starts indexing from the back
+print(manga[1:])  # shows everything every value after index 1 (included)
+# shows value from index 1 up to index 2 (1 & 2 and 3 is not included)
+print(manga[1:3])
