232deefb4779d145c9d4a9aa6440b43069f92dcc
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-03-01 22:21:59 +0100

    Numbers

---
+++ numbers.py
@@ -0,0 +1,19 @@
+from math import *
+
+my_num = 5
+print(2)
+print(2.0928)
+print(-2.0928)
+print(3 + 4.5)
+print(3 * (4 + 5))
+print(10 % 3)  # modulo
+print(my_num)
+print(str(my_num) + ' is my favorite number')  # num to string
+print(abs(my_num))  # absolute value
+print(pow(3, 2))  # 3**2
+print(max(3, 2))  # prints the bigger number
+print(min(3, 2))  # prints the lower number
+print(round(3.7))  # rounds up decimals
+print(floor(3.7))  # rounds and grabs the number down
+print(ceil(3.7))  # rounds and grabs the number up
+print(sqrt(36))  # returns the square root
