45fc1213ba8dd5835673fc8af6c040fb90aac880
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-03-01 22:05:23 +0100

    Strings

---
+++ strings.py
@@ -0,0 +1,9 @@
+phrase = "Python Lessons"
+print(phrase + " are cool")  # concatenation
+print(phrase.lower())  # lowercase
+print(phrase.upper())  # uppercase
+print(phrase.upper().isupper())  # boolean true if it's uppercase
+print(len(phrase))  # length of the string
+print(phrase[3])  # printing the 4th letter
+print(phrase.index("o"))  # looking for the index of a element
+print(phrase.replace("Python", "Javascript"))  # replacing a value
