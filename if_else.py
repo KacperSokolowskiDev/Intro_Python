7eec242a48ad525c38696c0e4153cb5c8d44bd77
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-03-03 23:45:36 +0100

    if/else

--- if_else.py
+++ if_else.py
@@ -10,3 +10,15 @@ elif not(is_male) and is_tall:
     print("You are tall but not a male")
 else:
     print("You are not a male and not tall")
+
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(300, 40, 501))
