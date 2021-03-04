a0ec7c123f0a863d9e3d3607b2a83c7d83be5777
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-03-04 01:01:40 +0100

    calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,14 @@
-# Basic version
 
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-result = float(num1) + float(num2)
-
-print(result)
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid Operator")
