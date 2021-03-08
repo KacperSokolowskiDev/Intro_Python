bafd8c08662f185317b7ef35554ea0500ae9468e
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-03-08 17:13:24 +0100

    2d lists, nested loop & function

---
+++ 2D_lists.py
@@ -0,0 +1,15 @@
+
+# 2D List of 4 rows and 3 columns
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+for row in number_grid:
+    for col in row:
+        print(col)
+
+print(number_grid[0][0])
+print(number_grid[2][1])
--- functions.py
+++ functions.py
@@ -6,9 +6,25 @@ say_hi("Kacper", 21)
 say_hi("Jeff", 33)
 
 
+print("-----")
+
+
 def cube(num):
     return num*num*num  # no statement works after the return, breaks the function
 
 
 result = cube(2)
 print(result)
+
+
+print("-----")
+
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+
+print(raise_to_power(2, 4))
