366fe4615fc9c3a13547c0e91a6b9c0ae785eced
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-05-27 20:15:13 +0200

    file managment

---
+++ files_test/various.py
@@ -0,0 +1,6 @@
+from os import *
+
+# mkdir("test") # make directory
+# chdir("test") # change directory
+# mkdir("newdir") # make new directory inside test directory
+rename("./files_test/manga.txt", "./files_test/myManga.txt") # renaming files
